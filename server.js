f92ca494f9038856f4c5805655b748b746cb6292
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-06-16 17:35:14 -0400

    set up browser live reload and dart sass, started setting up sass folder structure

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const sassMiddleware = require("express-dart-sass");
+const path = require("path");
+const connectLiveReload = require("connect-livereload");
+const livereload = require("livereload");
 const app = express();
+const liveReloadServer = livereload.createServer();
+
+liveReloadServer.watch(path.join(__dirname, "/public"));
+app.use(connectLiveReload());
+
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "src/scss"),
+    dest: path.join(__dirname, "public"),
+  })
+);
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html");
+});
 
 const PORT = process.env.PORT || 5500;
 
