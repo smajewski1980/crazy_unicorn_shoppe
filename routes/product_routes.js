c701c13d20b043589782de0139e8614336419573
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-07-08 12:37:17 -0400

    modified the query for /products to include inventory data, also updated the yaml file

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const path = require("path");
 const pool = require(path.join(__dirname, "../database/db_connect"));
 
+// returns all products
 router.get("/", async (req, res, next) => {
   try {
     const result = await pool.query(
-      "select * from products order by product_name"
+      "select * from products as p join inventory as i on p.product_id = i.product_id order by product_name"
     );
     res.status(200).send(result.rows);
   } catch (error) {
