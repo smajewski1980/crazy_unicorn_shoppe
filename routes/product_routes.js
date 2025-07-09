4f1b7bf9c66647e6acd59978211df75474d661a5
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-07-09 10:54:03 -0400

    created, tested and handled errors for PUT /products/:id endpoint

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -82,4 +82,40 @@ router.get("/:id", (req, res, next) => {
   );
 });
 
+router.put("/:id", (req, res, next) => {
+  const prod_id = req.params.id;
+  const {
+    product_name,
+    product_description,
+    product_price,
+    image_url,
+    category_id,
+  } = req.body;
+  pool.query(
+    "update products set product_name = $1, product_description = $2, product_price = $3, image_url = $4, category_id = $5 where product_id = $6 returning product_id",
+    [
+      product_name,
+      product_description,
+      product_price,
+      image_url,
+      category_id,
+      prod_id,
+    ],
+    (err, result) => {
+      if (!result.rows.length) {
+        const error = new Error(err || "not a valid product id");
+        error.status = 400;
+        next(error);
+        return;
+      }
+      res
+        .status(200)
+        .send({
+          msg: "successful update",
+          product_id: result.rows[0].product_id,
+        });
+    }
+  );
+});
+
 module.exports = router;
