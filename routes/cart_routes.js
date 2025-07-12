a2d4741b1fe3165e6599053e635781c50ab16c9a
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-07-11 21:37:17 -0400

    created PUT /cart endpoint, edited swagger file as needed

--- routes/cart_routes.js
+++ routes/cart_routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const path = require("path");
 const pool = require(path.join(__dirname, "../database/db_connect"));
 
+// ****there is no error handling yet********
+
 // adds a product to the cart
 router.post("/", async (req, res, next) => {
   if (req.user) {
@@ -52,6 +54,21 @@ router.get("/", async (req, res, next) => {
     return res.status(200).send(result.rows);
   }
   return res.status(401).send("you must be logged in to see your cart");
+});
+
+// update the quantity of a product in the cart
+router.put("/", async (req, res, next) => {
+  const currentUserId = req.user.user_id;
+  const { product_id, item_qty } = req.body;
+  const cartId = await pool.query(
+    "select cart_id from cart_items_with_names where user_id = $1",
+    [currentUserId]
+  );
+  const result = await pool.query(
+    "update cart_items set item_qty = $1 where product_id = $2 and cart_id = $3 returning *",
+    [item_qty, product_id, cartId.rows[0].cart_id]
+  );
+  return res.status(200).send(result.rows);
 });
 
 module.exports = router;
