6fcec4e75d223f978ee1071cf1a7f77e33b33773
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-07-18 21:21:15 -0400

    hooked up radio buttons to show the appropriate form fields when checked

--- public/add-new-product.js
+++ public/add-new-product.js
@@ -1,22 +1,48 @@
 const form = document.getElementById("add-product-form");
 const btnSubmit = document.getElementById("btn-add-prod-submit");
 const radioBtns = Array.from(document.querySelectorAll("input[type='radio']"));
+const idInputWrapper = document.getElementById("product-id-input-wrapper");
 
-function checkRadioBtns() {
-  let checked;
+function uncheckRadioBtns() {
   radioBtns.forEach((btn) => {
-    if (btn.checked) {
-      checked = btn.value;
-    }
+    btn.checked = false;
   });
-  return checked;
 }
+uncheckRadioBtns();
+
+const formChildren = Array.from(form.children);
+function changeInputDisplay(display) {
+  formChildren.forEach((item) => {
+    item.style.display = display;
+  });
+}
+
+function handleRadioBtn(e) {
+  switch (e.target.value) {
+    case "add-product":
+      changeInputDisplay("block");
+      idInputWrapper.style.display = "none";
+      break;
+    case "update-product":
+      changeInputDisplay("block");
+      break;
+    case "delete-product":
+      changeInputDisplay("none");
+      idInputWrapper.style.display = "block";
+      break;
+    default:
+      break;
+  }
+}
+
+radioBtns.forEach((btn) => {
+  btn.addEventListener("change", handleRadioBtn);
+});
 
 function handleFormSubmit(e) {
   e.preventDefault();
   const formData = new FormData(form);
   // console.log(formData);
-  console.log(checkRadioBtns());
   form.reset();
 }
 
