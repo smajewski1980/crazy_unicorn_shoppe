6c76a060e74577f9400f7420d19d6af09d3b8426
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-07-18 20:07:15 -0400

    added a set of radio btns to choose add/update/de;ete in add product form

--- public/add-new-product.js
+++ public/add-new-product.js
@@ -1,10 +1,22 @@
 const form = document.getElementById("add-product-form");
 const btnSubmit = document.getElementById("btn-add-prod-submit");
+const radioBtns = Array.from(document.querySelectorAll("input[type='radio']"));
+
+function checkRadioBtns() {
+  let checked;
+  radioBtns.forEach((btn) => {
+    if (btn.checked) {
+      checked = btn.value;
+    }
+  });
+  return checked;
+}
 
 function handleFormSubmit(e) {
   e.preventDefault();
   const formData = new FormData(form);
-  console.log(formData);
+  // console.log(formData);
+  console.log(checkRadioBtns());
   form.reset();
 }
 
