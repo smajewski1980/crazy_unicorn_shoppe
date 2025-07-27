24bb0c0d11a36fb3c90cc3880d955fd36a4321da
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-07-27 18:29:08 -0400

    repeated the ast commit on the shop page

--- public/shop.js
+++ public/shop.js
@@ -233,3 +233,69 @@ document.addEventListener('click', (e) => {
 btnAddToCart.addEventListener('click', handleAddItem);
 
 getAllProducts();
+
+const navFirstItem = document.querySelector('nav ul li:first-child');
+
+async function getUser() {
+  try {
+    const response = fetch('/user/status');
+    const userData = (await response).json();
+    return userData;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function styleLogOut() {
+  navFirstItem.style.rotate = '0deg';
+  navFirstItem.querySelector('p:first-child').style.margin = '0';
+  // navFirstItem.querySelector('p:last-child').style.margin = '0';
+  navFirstItem.style.display = 'grid';
+  navFirstItem.style.placeItems = 'center';
+}
+
+async function isLoggedIn() {
+  // if a user is logged in on page load, load appropriate UI
+  try {
+    const response = await getUser();
+    // when i set up the endpoint, i had it return {msg: not authenticated} if not logged in
+    if (!response.msg) {
+      console.log('user is logged in');
+      navFirstItem.innerHTML = `
+        <p>Logged in as ${response.name}.</p>
+        <button id="logout-link">LOGOUT</button>
+      `;
+      document
+        .getElementById('logout-link')
+        .addEventListener('click', handleLogout);
+      styleLogOut();
+      return;
+    }
+    console.log('user is not logged in');
+    navFirstItem.innerHTML = `
+            <a href="./signin.html">
+              <img
+                src="./assets/images/button.webp"
+                alt=""
+              />
+            </a>
+    `;
+    return;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handleLogout() {
+  const user = await getUser();
+  const name = await user.name;
+  fetch('/user/logout');
+  if (user.msg) {
+    alert('There is no one logged in.');
+    return;
+  }
+  alert(`${name} is now logged out`);
+  window.location.reload();
+}
+
+isLoggedIn();
