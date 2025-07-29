17a7eca108d813510c86ff2b5ddc80f52a3b0f55
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-07-28 20:59:39 -0400

    generated a table with invoice data on the checkout page

---
+++ public/checkout.js
@@ -0,0 +1,47 @@
+const invoiceTableBody = document.getElementById('invoice-table-body');
+const invoiceTableFoot = document.getElementById('invoice-table-foot');
+
+async function getCheckoutData() {
+  try {
+    const response = await fetch('/cart/checkout');
+    const data = await response.json();
+    populateCheckoutData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function populateCheckoutData(data) {
+  console.log(data);
+  let htmlString = '';
+  let invoiceSubtotal = 0;
+  data.forEach((item) => {
+    invoiceSubtotal += item.item_qty * item.product_price;
+    htmlString += `
+      <tr><td>${item.item_qty}</td><td>${item.product_name}</td><td>$${
+      item.product_price
+    }</td><td>$${item.item_qty * item.product_price}</td></tr>
+    `;
+  });
+  invoiceTableBody.innerHTML = htmlString;
+
+  invoiceTableFoot.innerHTML = `<tr><th>Invoice Subtotal</th><td></td><td></td><td>$${invoiceSubtotal}</td></tr>`;
+  invoiceTableFoot.innerHTML += `<tr><th>Shipping</th><td></td><td></td><td>${
+    invoiceSubtotal > 99 ? 'FREE' : `$${invoiceSubtotal * 0.1}`
+  }</td></tr>`;
+
+  const taxCalc =
+    (invoiceSubtotal > 99 ? 0 : invoiceSubtotal * 0.1) + invoiceSubtotal;
+
+  invoiceTableFoot.innerHTML += `<tr><th>Sales Tax</th><td></td><td></td><td>$${
+    taxCalc * 0.08
+  }</td></tr>`;
+
+  const total =
+    taxCalc * 0.08 +
+    (invoiceSubtotal > 99 ? 0 : invoiceSubtotal * 0.1) +
+    invoiceSubtotal;
+  invoiceTableFoot.innerHTML += `<tr><th>Total</th><td></td><td></td><td>$${total}</td></tr>`;
+}
+
+getCheckoutData();
