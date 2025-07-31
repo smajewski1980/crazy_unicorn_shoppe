78596a04cdcd3a9ad34166a63de8e5a919c4356c
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-07-31 12:40:16 -0400

    modified the GET /order/:id/all endpoint

--- routes/order_routes.js
+++ routes/order_routes.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const path = require("path");
-const pool = require(path.join(__dirname, "../database/db_connect"));
-const isAuth = require("../middleware/is_auth");
+const path = require('path');
+const pool = require(path.join(__dirname, '../database/db_connect'));
+const isAuth = require('../middleware/is_auth');
 
 // creates a new order and gets an order_id
-router.post("/", isAuth, async (req, res, next) => {
+router.post('/', isAuth, async (req, res, next) => {
   const { user_id, order_total, payment_method, cart_id } = req.body;
 
   // this transaction will rollback if there is a problem
@@ -15,13 +15,13 @@ router.post("/", isAuth, async (req, res, next) => {
     const client = await pool.connect();
     try {
       const result = await client.query(
-        "INSERT INTO orders(user_id, order_total, payment_method, cart_id) VALUES($1, $2, $3, $4) RETURNING order_id",
-        [user_id, order_total, payment_method, cart_id]
+        'INSERT INTO orders(user_id, order_total, payment_method, cart_id) VALUES($1, $2, $3, $4) RETURNING order_id',
+        [user_id, order_total, payment_method, cart_id],
       );
       // adjust the products inventory
       const cartItemsResult = await client.query(
-        "SELECT * FROM cart_items WHERE cart_id = $1",
-        [cart_id]
+        'SELECT * FROM cart_items WHERE cart_id = $1',
+        [cart_id],
       );
       const cartItems = cartItemsResult.rows;
 
@@ -29,25 +29,25 @@ router.post("/", isAuth, async (req, res, next) => {
         const id = item.product_id;
         const qty = item.item_qty;
         await client.query(
-          "UPDATE inventory SET current_qty = current_qty - $1 WHERE product_id = $2",
-          [qty, id]
+          'UPDATE inventory SET current_qty = current_qty - $1 WHERE product_id = $2',
+          [qty, id],
         );
       });
 
       await client.query(
-        "UPDATE carts SET is_active = false WHERE cart_id = $1",
-        [cart_id]
+        'UPDATE carts SET is_active = false WHERE cart_id = $1',
+        [cart_id],
       );
 
-      await client.query("commit");
+      await client.query('commit');
       return res.status(200).send({
         order_id: result.rows[0].order_id,
-        msg: "order created",
+        msg: 'order created',
       });
     } catch (err) {
       const error = new Error(err);
-      await client.query("ROLLBACK");
-      error.message = err.detail + " Please try again.";
+      await client.query('ROLLBACK');
+      error.message = err.detail + ' Please try again.';
       return next(error);
     } finally {
       await client.release();
@@ -58,7 +58,7 @@ router.post("/", isAuth, async (req, res, next) => {
 });
 
 // get order info by id
-router.get("/:id", isAuth, async (req, res, next) => {
+router.get('/:id', isAuth, async (req, res, next) => {
   const order_id = req.params.id;
 
   const currentUser = req.user.user_id;
@@ -66,23 +66,23 @@ router.get("/:id", isAuth, async (req, res, next) => {
   try {
     // get the order info
     const result = await pool.query(
-      "SELECT * FROM orders WHERE order_id = $1",
-      [order_id]
+      'SELECT * FROM orders WHERE order_id = $1',
+      [order_id],
     );
     if (result.rowCount === 0) {
-      throw new Error("We could not find a order with that id.");
+      throw new Error('We could not find a order with that id.');
     }
     // make sure the order id belongs to the current user
     if (result.rows[0].user_id !== currentUser) {
       throw new ReferenceError(
-        "Mind your business, that is someone else's order id!"
+        "Mind your business, that is someone else's order id!",
       );
     }
     // get and attach the order items to the result
     const cart_id = result.rows[0].cart_id;
     const orderItemsResult = await pool.query(
-      "SELECT * FROM order_items_lookup WHERE cart_id = $1",
-      [cart_id]
+      'SELECT * FROM order_items_lookup WHERE cart_id = $1',
+      [cart_id],
     );
 
     result.rows[0].order_items = await orderItemsResult.rows;
@@ -106,7 +106,7 @@ router.get("/:id", isAuth, async (req, res, next) => {
 });
 
 // update the order status
-router.put("/:id", isAuth, async (req, res, next) => {
+router.put('/:id', isAuth, async (req, res, next) => {
   const orderId = req.params.id;
   const currentUser = req.user.user_id;
   const updatedOrderStatus = req.body.updatedOrderStatus;
@@ -114,28 +114,28 @@ router.put("/:id", isAuth, async (req, res, next) => {
   try {
     // get the order's user id
     const orderUserId = await pool.query(
-      "SELECT user_id FROM orders WHERE order_id = $1",
-      [orderId]
+      'SELECT user_id FROM orders WHERE order_id = $1',
+      [orderId],
     );
     // if the order id doesn't exist
     if (orderUserId.rowCount === 0) {
-      const error = new Error("We could not find an order with that id.");
+      const error = new Error('We could not find an order with that id.');
       throw error;
     }
     // see if the current user is the order's user
     if (currentUser !== orderUserId.rows[0].user_id) {
       const error = new ReferenceError(
-        "Mind your business, that is someone else's order id!"
+        "Mind your business, that is someone else's order id!",
       );
       throw error;
     }
     // update the order status
     const result = await pool.query(
-      "UPDATE orders SET order_status = $1 WHERE order_id = $2 RETURNING order_status",
-      [updatedOrderStatus, orderId]
+      'UPDATE orders SET order_status = $1 WHERE order_id = $2 RETURNING order_status',
+      [updatedOrderStatus, orderId],
     );
     return res.status(200).send({
-      msg: "order status has been updated",
+      msg: 'order status has been updated',
       order_status: result.rows[0].order_status,
     });
   } catch (err) {
@@ -152,7 +152,7 @@ router.put("/:id", isAuth, async (req, res, next) => {
 });
 
 // cancel an order
-router.delete("/:id", isAuth, async (req, res, next) => {
+router.delete('/:id', isAuth, async (req, res, next) => {
   // i would think in a real world app, this would be where we would put the code for the payment processor to handle a refund, therefore this endpoint will just change the status of the order to canceled and put the items back in inventory.
 
   // put items back into inventory
@@ -166,38 +166,38 @@ router.delete("/:id", isAuth, async (req, res, next) => {
     const client = await pool.connect();
 
     try {
-      await client.query("BEGIN");
+      await client.query('BEGIN');
       // get the order's user id
       const orderUserId = await client.query(
-        "SELECT user_id FROM orders WHERE order_id = $1",
-        [orderId]
+        'SELECT user_id FROM orders WHERE order_id = $1',
+        [orderId],
       );
       // if the order id doesn't exist
       if (orderUserId.rowCount === 0) {
-        const error = new Error("We could not find an order with that id.");
+        const error = new Error('We could not find an order with that id.');
         throw error;
       }
       // see if the current user is the order's user
       if (currentUser !== orderUserId.rows[0].user_id) {
         const error = new ReferenceError(
-          "Mind your business, that is someone else's order id!"
+          "Mind your business, that is someone else's order id!",
         );
         throw error;
       }
       // update the order status
       const result = await client.query(
         "UPDATE orders SET order_status = 'canceled' WHERE order_id = $1 RETURNING order_status",
-        [orderId]
+        [orderId],
       );
       // put the items back into inventory
       const cartId = await client.query(
-        "SELECT cart_id FROM orders WHERE order_id = $1",
-        [orderId]
+        'SELECT cart_id FROM orders WHERE order_id = $1',
+        [orderId],
       );
 
       const cartItemsResult = await client.query(
-        "SELECT * FROM cart_items WHERE cart_id = $1",
-        [cartId.rows[0].cart_id]
+        'SELECT * FROM cart_items WHERE cart_id = $1',
+        [cartId.rows[0].cart_id],
       );
       const cartItems = cartItemsResult.rows;
 
@@ -205,28 +205,28 @@ router.delete("/:id", isAuth, async (req, res, next) => {
         const id = item.product_id;
         const qty = item.item_qty;
         await client.query(
-          "UPDATE inventory SET current_qty = current_qty + $1 WHERE product_id = $2",
-          [qty, id]
+          'UPDATE inventory SET current_qty = current_qty + $1 WHERE product_id = $2',
+          [qty, id],
         );
       });
 
-      await client.query("COMMIT");
+      await client.query('COMMIT');
       return res.status(200).send({
-        msg: "The order has been successfully canceled.",
+        msg: 'The order has been successfully canceled.',
         order_status: result.rows[0].order_status,
       });
     } catch (err) {
       if (err instanceof ReferenceError) {
         err.status = 401;
-        await client.query("ROLLBACK");
+        await client.query('ROLLBACK');
         return next(err);
       }
       if (err instanceof Error) {
         err.status = 404;
-        await client.query("ROLLBACK");
+        await client.query('ROLLBACK');
         return next(err);
       }
-      await client.query("ROLLBACK");
+      await client.query('ROLLBACK');
       return next(err);
     } finally {
       await client.release();
@@ -237,18 +237,18 @@ router.delete("/:id", isAuth, async (req, res, next) => {
 });
 
 // returns order info for all orders for a user
-router.get("/:id/all", isAuth, async (req, res, next) => {
+router.get('/:id/all', isAuth, async (req, res, next) => {
   const userId = req.params.id;
 
   // get all the users orders
   const result = await pool.query(
-    "SELECT * FROM get_all_orders WHERE user_id = $1",
-    [userId]
+    'SELECT * FROM summerized_orders_list WHERE user_id = $1',
+    [userId],
   );
 
   // if there are no orders for that user
   if (result.rowCount === 0) {
-    const error = new Error("We could not find any orders for that user.");
+    const error = new Error('We could not find any orders for that user.');
     error.status = 404;
     return next(error);
   }
@@ -260,46 +260,7 @@ router.get("/:id/all", isAuth, async (req, res, next) => {
     return next(error);
   }
 
-  // this is the final result to be sent back
-  const refinedResult = [];
-  // used to ensure the main order data only goes into refined result once
-  const orderIdTracker = [];
-
-  result.rows.forEach((row) => {
-    // if this order hasn't been seen by this loop yet
-    if (!orderIdTracker.includes(row.order_id)) {
-      orderIdTracker.push(row.order_id);
-
-      const orderObj = {
-        orderId: row.order_id,
-        userId: row.user_id,
-        orderDate: row.order_date,
-        orderTotal: parseInt(row.order_total),
-        orderStatus: row.order_status,
-        paymentMethod: row.payment_method,
-        freeShipping: row.free_shipping_elligible,
-        orderItems: [],
-      };
-
-      refinedResult.push(orderObj);
-    }
-
-    // find the index in refined result of this order
-    const idx = refinedResult.findIndex((item) => {
-      return item.orderId === row.order_id;
-    });
-    // creates an object for the current order item
-    const orderItem = {
-      productName: row.product_name,
-      productPrice: parseInt(row.product_price),
-      itemQty: row.item_qty,
-      subtotal: parseInt(row.subtotal),
-    };
-    // adds the current order item to the correct order's orderItems array
-    refinedResult[idx].orderItems.push(orderItem);
-  });
-
-  res.status(200).send(refinedResult);
+  res.status(200).send(result.rows);
 });
 
 module.exports = router;
