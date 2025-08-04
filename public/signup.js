ac79726f1078cf3826c758c11d2429c504d0ca2d
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-08-04 17:31:59 -0400

    added client side validation to forms, and styled accordingly

--- public/signup.js
+++ public/signup.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('sign-up-form');
 const btnSignup = document.getElementById('btn-signup');
+const pw = document.getElementById('input-password');
+const confpw = document.getElementById('input-conf-password');
 
 async function sendFormData(data) {
   const options = {
@@ -11,8 +13,10 @@ async function sendFormData(data) {
   };
 
   const response = await fetch('/user/register', options);
-  const resData = await response.json();
-  console.log(resData);
+  // const resData = await response.json();
+  if (response.ok) {
+    window.location.href = './signin.html';
+  }
 }
 
 function handleSignup(e) {
@@ -20,7 +24,24 @@ function handleSignup(e) {
   const formData = new FormData(form);
   const dataObject = Object.fromEntries(formData);
   sendFormData(dataObject);
-  form.reset();
+  // form.reset();
 }
 
 btnSignup.addEventListener('click', handleSignup);
+
+function doPasswordsMatch() {
+  const password = pw.value;
+  const confPassword = confpw.value;
+  const match = password === confPassword;
+
+  if (match) {
+    confpw.setCustomValidity('');
+    confpw.classList.add('validity-style');
+  } else {
+    confpw.setCustomValidity('passwords dont match');
+  }
+
+  // console.log(match);
+}
+
+confpw.addEventListener('input', doPasswordsMatch);
