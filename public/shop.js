f4ea877e599e4fec4475c0802b0bd422b27e6da4
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-08-12 10:18:58 -0400

    added a back to top button that shows up when you scroll down on the shopping page

--- public/shop.js
+++ public/shop.js
@@ -417,3 +417,18 @@ async function setCartItemQty() {
     throw new Error(error);
   }
 }
+
+// back to top button
+const scrollDistance = () => window.scrollY;
+const upArrow = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  let currentScroll = scrollDistance();
+  if (currentScroll > 300) {
+    upArrow.style.opacity = '1';
+    upArrow.style.pointerEvents = 'auto';
+  } else {
+    upArrow.style.opacity = '0';
+    upArrow.style.pointerEvents = 'none';
+  }
+});
