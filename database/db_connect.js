d3f3e0f7153f1b666dafd13601159e21309ffb89
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-08-21 21:42:15 -0400

    hooked up the cloud database

--- database/db_connect.js
+++ database/db_connect.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const pg = require('pg');
 const { Pool } = pg;
 const pool = new Pool({
-  user: 'postgres',
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: 'localhost',
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
