feb82482cf3b4f4a37f5b2731262d28ddf1d212b
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-08-27 12:45:10 -0400

    hooked up front end for leaving a thought and handled errors

---
+++ public/thought.js
@@ -0,0 +1,38 @@
+import { toasty } from './utils/toasty.js';
+const nameInput = document.getElementById('name');
+const thoughtInput = document.getElementById('thought');
+const btnThought = document.getElementById('btn-thought');
+
+async function handleThought(e) {
+  e.preventDefault();
+  const name = nameInput.value;
+  const thought = thoughtInput.value;
+
+  // check for both fields being filled out
+  if (!name || !thought) {
+    return toasty('Both fields need to be filled out please.');
+  }
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({ name, thought }),
+  };
+
+  const response = await fetch('./user/thought', options);
+
+  if (response.ok) {
+    toasty('Your thought has been submitted.', 'home');
+    return;
+  } else if (response.status === '500') {
+    return toasty('There was a server problem, please try again.');
+  }
+
+  // handle any server anomolies
+  const msg = await response.json();
+  return toasty(await msg);
+}
+
+btnThought.addEventListener('click', handleThought);
