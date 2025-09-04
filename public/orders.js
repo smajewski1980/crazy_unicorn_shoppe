bbbdb143160470424e7c9627541afeee59432c60
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-09-03 21:12:57 -0400

    still trying to figure out a problem with the dates on the order page

--- public/orders.js
+++ public/orders.js
@@ -15,9 +15,9 @@ const btnOrderCancel = document.getElementById('btn-cancel-order');
 function formatDate(date) {
   const dateObj = new Date(date);
 
-  const month = dateObj.getUTCMonth() + 1;
-  const day = dateObj.getUTCDate();
-  const year = dateObj.getUTCFullYear();
+  const month = dateObj.getMonth() + 1;
+  const day = dateObj.getDate();
+  const year = dateObj.getFullYear();
 
   return `${month}-${day}-${year}`;
 }
