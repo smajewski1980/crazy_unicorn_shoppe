fe949271b8097aaf64b4c29bb95e7f47a88bb99f
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-09-22 10:05:35 -0400

    wrote test for GET /products:id

--- routes/product_routes.test.js
+++ routes/product_routes.test.js
@@ -1,8 +1,22 @@
 const request = require('supertest');
 
+// walk before we run
+// the 'GET' tests
+
 test('get all products', async () => {
-  await request('localhost:4700')
+  const res = await request('localhost:4700')
     .get('/products')
     .expect(200)
     .expect('Content-Type', 'application/json; charset=utf-8');
+
+  expect(Object.keys(res.body).length).toBeTruthy();
+});
+
+test('get a product by id', async () => {
+  const res = await request('localhost:4700')
+    .get('/products/1')
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8');
+
+  expect(res.body.product_id).toBe(1);
 });
