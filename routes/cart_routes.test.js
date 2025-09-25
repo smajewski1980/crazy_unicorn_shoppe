c347c71bc836649d1f89d373700c5702e2620e3a
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-09-25 19:19:59 -0400

    created test for GET /cart/checkout

--- routes/cart_routes.test.js
+++ routes/cart_routes.test.js
@@ -15,37 +15,46 @@ const fullCartUser = {
 };
 
 describe('cart_routes', () => {
-  describe('endpoints that need auth', () => {
-    describe('the GET endpoints', () => {
-      test('returns 401 if not logged in', async () => {
-        await request('localhost:4700').get('/cart').expect(401);
-      });
+  describe('the GET endpoints', () => {
+    test('returns 401 if not logged in', async () => {
+      await request('localhost:4700').get('/cart').expect(401);
+    });
 
-      test('returns 400 if the user has an empty cart', async () => {
-        const agent = superagent.agent();
-        await agent
-          .post('http://localhost:4700/user/login')
-          .send(emptyCartUser);
+    test('returns 400 if the user has an empty cart', async () => {
+      const agent = superagent.agent();
+      await agent.post('http://localhost:4700/user/login').send(emptyCartUser);
 
-        const res = await agent
-          .get('http://localhost:4700/cart')
-          .ok((res) => res.status === 400)
-          .then((res) => res);
+      const res = await agent
+        .get('http://localhost:4700/cart')
+        .ok((res) => res.status === 400)
+        .then((res) => res);
 
-        expect(res.statusCode).toBe(400);
-        expect(res.text).toBe('Thats one empty cart you got there!');
-      });
+      expect(res.statusCode).toBe(400);
+      expect(res.text).toBe('Thats one empty cart you got there!');
+    });
 
-      test('returns user cart data', async () => {
-        const agent = superagent.agent();
-        await agent.post('http://localhost:4700/user/login').send(fullCartUser);
+    test('returns user cart data', async () => {
+      const agent = superagent.agent();
+      await agent.post('http://localhost:4700/user/login').send(fullCartUser);
 
-        const res = await agent.get('http://localhost:4700/cart');
-        expect(res.statusCode).toBe(200);
-        expect(res.headers['content-type']).toBe(
-          'application/json; charset=utf-8',
-        );
-      });
+      const res = await agent.get('http://localhost:4700/cart');
+
+      expect(res.statusCode).toBe(200);
+      expect(res.headers['content-type']).toBe(
+        'application/json; charset=utf-8',
+      );
+    });
+
+    test('gets the users checkout data', async () => {
+      const agent = superagent.agent();
+      await agent.post('http://localhost:4700/user/login').send(fullCartUser);
+
+      const res = await agent.get('http://localhost:4700/cart/checkout');
+
+      expect(res.statusCode).toBe(200);
+      expect(res.headers['content-type']).toBe(
+        'application/json; charset=utf-8',
+      );
     });
   });
 });
