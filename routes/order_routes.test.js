471e245776161d8a517dcfbb3b1f2df758e57872
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-10-02 14:45:40 -0400

    added the non-happy path tests for GET /order/:id

--- routes/order_routes.test.js
+++ routes/order_routes.test.js
@@ -1,15 +1,50 @@
 const request = require('supertest');
 const superagent = require('superagent');
 
+const BASE_URL = 'http://localhost:4700';
+
+const goodUser = {
+  username: 'Homer Simpson',
+  password: 'doh_nuts',
+};
+
+const getAgent = () => superagent.agent();
+
+async function logIn(agent) {
+  const loginRes = await agent.post(BASE_URL + '/user/login').send(goodUser);
+  return loginRes.statusCode;
+}
+
 describe('GET endpoints', () => {
   describe('/order/:id', () => {
-    test.todo('returns 401 if not auth');
-    test.todo(
-      'returns 404 error message: We could not find a order with that id. if given a bad id',
-    );
-    test.todo(
-      "returns 401 error message: Mind your business, that is someone else's order id! if :id is not of the logged in user",
-    );
+    test('returns 401 if not auth', async () => {
+      const res = await request(BASE_URL).get('/order/47');
+      expect(res.statusCode).toBe(401);
+    });
+
+    test('returns 404 error message: We could not find a order with that id. if given a bad id', async () => {
+      const agent = getAgent();
+      expect(await logIn(agent)).toBe(200);
+
+      const res = await agent
+        .get(BASE_URL + '/order/99999')
+        .ok((res) => res.statusCode === 404);
+      expect(res.statusCode).toBe(404);
+      expect(res.error.text).toBe('"We could not find a order with that id."');
+    });
+
+    test("returns 401 error message: Mind your business, that is someone else's order id! if :id is not of the logged in user", async () => {
+      const agent = getAgent();
+      expect(await logIn(agent)).toBe(200);
+
+      const res = await agent
+        .get(BASE_URL + '/order/2')
+        .ok((res) => res.statusCode === 401);
+      expect(res.statusCode).toBe(401);
+      expect(res.error.text).toBe(
+        '"Mind your business, that is someone else\'s order id!"',
+      );
+    });
     test.todo('returns order data when provided a valid order id');
   });
 
