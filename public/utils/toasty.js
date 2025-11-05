aabf1ced28b5a100a9f0889246480cfbaadb162d
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-11-04 19:06:09 -0500

    changed the duration of the toast messages

--- public/utils/toasty.js
+++ public/utils/toasty.js
@@ -1,34 +1,34 @@
 export function toasty(message, cb) {
   Toastify({
     text: message,
-    duration: 3500,
-    gravity: 'top',
-    position: 'left',
+    duration: 2000,
+    gravity: "top",
+    position: "left",
     close: true,
     style: {
-      background: 'linear-gradient(rgba(255, 188, 220, 0.5))',
-      backdropFilter: 'blur(3px)',
-      fontFamily: 'systemUi, sans-serif',
-      fontSize: '1.25rem',
-      paddingBlock: '1.5rem',
-      borderRadius: '15px 0px 15px',
-      color: 'black',
+      background: "linear-gradient(rgba(255, 188, 220, 0.5))",
+      backdropFilter: "blur(3px)",
+      fontFamily: "systemUi, sans-serif",
+      fontSize: "1.25rem",
+      paddingBlock: "1.5rem",
+      borderRadius: "15px 0px 15px",
+      color: "black",
     },
     callback: () => {
       if (!cb) {
         return;
-      } else if (cb === 'shop') {
-        window.location.href = '/shop.html';
-      } else if (cb === 'home') {
-        window.location.href = '/index.html';
-      } else if (cb === 'reload') {
+      } else if (cb === "shop") {
+        window.location.href = "/shop.html";
+      } else if (cb === "home") {
+        window.location.href = "/index.html";
+      } else if (cb === "reload") {
         window.location.reload();
-      } else if (cb === 'poly') {
+      } else if (cb === "poly") {
         document
-          .getElementById('product-dialog')
+          .getElementById("product-dialog")
           .style.setProperty(
-            '--clip-path-poly',
-            'polygon(29rem 0rem, 29rem 0rem, 100% 0rem, 100% 100%, 0% 100%, 0% 0rem)',
+            "--clip-path-poly",
+            "polygon(29rem 0rem, 29rem 0rem, 100% 0rem, 100% 100%, 0% 100%, 0% 0rem)",
           );
       }
     },
